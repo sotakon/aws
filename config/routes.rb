15dfd93e4613718681283cf0076b38d1bbeb4807
sotakon <skon1221@gmail.com>
2021-03-06 18:17:16 +0900

    root変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :blogs
+  root  'blogs#index'  #この1行を追加
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
